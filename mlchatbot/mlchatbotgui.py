1c0bcf3bb1f78a66576aa1f1affb882814c24598
Johnnie <jmered01@rams.shepherd.edu>
2020-04-02 10:21:51 -0400

    fixed

--- mlchatbot/mlchatbotgui.py
+++ mlchatbot/mlchatbotgui.py
@@ -2,6 +2,7 @@ import nltk
 from tkinter import *
 from tkinter import scrolledtext
 from tkinter import font as tkFont
+from tensorboard import *
 from nltk.stem.lancaster import LancasterStemmer
 from nltk.corpus import stopwords
 stemmer = LancasterStemmer()
@@ -13,17 +14,8 @@ import random
 import time
 import pickle
 import json
-window = Tk()
-window.title("Chatbot App")
-window.configure(bg= '#B8BCFF')
-helv36 = tkFont.Font(family='Helvetica', size=20, weight='bold')
-lbl1 = scrolledtext.ScrolledText(window,width=80,height=20,wrap=WORD)
-lbl1["font"] = helv36
-lbl1.grid(column=0,padx=20,pady=10,row=1,columnspan = 3,sticky=NW)
-window.geometry('1300x900')
-txt1 = Entry(window, width=50)
-txt1["font"] = helv36
-txt1.grid(column=0,padx=20,pady=10, row=2)
+import time
+
 
 def hitenter(self):
     clicked()
@@ -57,112 +49,114 @@ def clicked():
     lbl1.insert(INSERT,"\n\n\n")
     txt1.delete(0, END)
     lbl1.see("end")
+  
 
-btn = Button(window, text="Chat",padx = 45, pady = 20, command=clicked)
-btn['font'] = helv36
-btn.configure(bg='#D1D4FF')
-btn.grid(column=1, ipadx=0,ipady=0, row=2)
-window.bind('<Return>', hitenter)
-
-btnexit = Button(window, text="Exit",padx = 45, pady = 20, command=lambda: window.destroy())
-btnexit['font'] = helv36
-btnexit.configure(bg='#D1D4FF')
-btnexit.grid(column=2, row=2)
-
-
-""" with open(os.path.abspath("C:/Users/Johnnie5Dubv/Documents/chatbot/Capstone-Chatbot/mlchatbot/json stuff/intents.json"),encoding="utf8") as file:
-    data = json.load(file) """
-#nltk.download('stopwords')
-#print(data)            prints the whole file
-#print(data["intents"]) prints the dictionary
-""" try:
-    with open("./data.pickle", "rb") as f:
-        words, labels, training, output = pickle.load(f)
+def chatbot():
+    """ with open(os.path.abspath("C:/Users/Johnnie5Dubv/Documents/chatbot/Capstone-Chatbot/mlchatbot/json stuff/intents.json"),encoding="utf8") as file:
+        data = json.load(file) """
+    #nltk.download('stopwords')
+    #print(data)            prints the whole file
+    #print(data["intents"]) prints the dictionary
+    """ try:
+        with open("./data.pickle", "rb") as f:
+            words, labels, training, output = pickle.load(f)
     
-except: """
-words = []
-labels = []
-docs_x = []
-docs_y = []
-""" 
-for intent in data["intents"]:
-    for pattern in intent["patterns"]:
-        wrds = nltk.word_tokenize(pattern)
+    except: """
+    global words
+    global data
+    global training
+    global output
+    global labels
+    global model
+    words = []
+    labels = []
+    docs_x = []
+    docs_y = []
+    training = []
+    output = []
+    
+    """ 
+    for intent in data["intents"]:
+        for pattern in intent["patterns"]:
+            wrds = nltk.word_tokenize(pattern)
+            words.extend(wrds)
+            docs_x.append(wrds)
+            docs_y.append(intent["tag"])
+            if intent["tag"] not in labels:
+                labels.append(intent["tag"]) """
+    with open(os.path.abspath("C:/Users/Johnnie5Dubv/Documents/chatbot/Capstone-Chatbot/json stuff/QAjson.json"),encoding="utf8") as file:
+        data = json.load(file)
+    for intent in data["intents"]:
+        #for pattern in intent["patterns"]:
+        wrds = nltk.word_tokenize(intent["patterns"])
         words.extend(wrds)
         docs_x.append(wrds)
         docs_y.append(intent["tag"])
-        if intent["tag"] not in labels:
-            labels.append(intent["tag"]) """
-with open(os.path.abspath("C:/Users/Johnnie5Dubv/Documents/chatbot/Capstone-Chatbot/json stuff/QAjson.json"),encoding="utf8") as file:
-    data = json.load(file)
-for intent in data["intents"]:
-    #for pattern in intent["patterns"]:
-    wrds = nltk.word_tokenize(intent["patterns"])
-    words.extend(wrds)
-    docs_x.append(wrds)
-    docs_y.append(intent["tag"])
-    #if intent["tag"] not in labels:
-    labels.append(intent["tag"])
-words = [stemmer.stem(w.lower()) for w in words if w not in set(stopwords.words("english"))]
-words = sorted(list(set(words)))
-print(words)
-labels = sorted(labels)
-#print(labels)  #prints a list of our tags from intents file
-training = []
-output = []
+        #if intent["tag"] not in labels:
+        labels.append(intent["tag"])
+    words = [stemmer.stem(w.lower()) for w in words if w not in set(stopwords.words("english")) and w != '?']
+    words = sorted(list(set(words)))
+    print(words)
+    labels = sorted(labels)
+    #print(labels)  #prints a list of our tags from intents file
+    
 
-out_empty = [0 for _ in range(len(labels))]
+    out_empty = [0 for _ in range(len(labels))]
 
-for x, doc in enumerate(docs_x):
-    bag = []
-    wrds = [stemmer.stem(w.lower()) for w in doc]
+    for x, doc in enumerate(docs_x):
+        bag = []
+        wrds = [stemmer.stem(w.lower()) for w in doc if w not in set(stopwords.words("english")) and w != '?']
 
-    for w in words:
-        if w in wrds:
-            bag.append(1)
-        else:
-            bag.append(0)
+        for w in words:
+            if w in wrds:
+                bag.append(1)
+            else:
+                bag.append(0)
 
-    output_row = out_empty[:]
-    output_row[labels.index(docs_y[x])] = 1
+        output_row = out_empty[:]
+        output_row[labels.index(docs_y[x])] = 1
 
-    training.append(bag)
-    output.append(output_row)  
-print(training)
-training = numpy.array(training)
-output = numpy.array(output)
-print(labels)
-print(output)
+        training.append(bag)
+        output.append(output_row)  
+    
+    training = numpy.array(training)
+    output = numpy.array(output)
 
-with open("data.pickle", "wb") as f:
-    pickle.dump((words, labels, training, output), f)
+    """ print(training)
+    print(labels)
+    print(output) """
+    NAME = "Chatbot-model-{}".format(int(time.time()))
+    with open("data.pickle", "wb") as f:
+        pickle.dump((words, labels, training, output), f)
 
-tensorflow.reset_default_graph()
+    tensorflow.reset_default_graph()
 
-net = tflearn.input_data(shape=[None, len(training[0])])
-net = tflearn.fully_connected(net, 20)
-#net = tflearn.dropout(net, 0.1)
-net = tflearn.fully_connected(net, 10)
-#net = tflearn.dropout(net, 0.01)
-net = tflearn.fully_connected(net, 20)
-#net = tflearn.dropout(net, 0.1)
-net = tflearn.fully_connected(net, len(output[0]), activation="softmax")
-net = tflearn.regression(net)
-model = tflearn.DNN(net) 
+    net = tflearn.input_data(shape=[None, len(training[0])])
+    net = tflearn.fully_connected(net, 20)
+    #net = tflearn.dropout(net, 0.1)
+    net = tflearn.fully_connected(net, 10)
+    #net = tflearn.dropout(net, 0.01)
+    net = tflearn.fully_connected(net, 20)
+    #net = tflearn.dropout(net, 0.1)
+    net = tflearn.fully_connected(net, len(output[0]), activation="softmax")
+    net = tflearn.regression(net)
+    model = tflearn.DNN(net, tensorboard_dir='./tfboardlog/{}'.format(NAME),tensorboard_verbose = 3) 
 
 
-""" try:
-    model.load(os.path.abspath("C:/Users/Johnnie5Dubv/Documents/chatbot/Capstone-Chatbot/mlchatbot/model.tflearn")) 
-except: """
-model.fit(training, output, n_epoch=1, batch_size=16, show_metric=True)
-model.save(os.path.abspath("C:/Users/Johnnie5Dubv/Documents/chatbot/Capstone-Chatbot/mlchatbot/model.tflearn"))
+    """ try:
+        model.load(os.path.abspath("C:/Users/Johnnie5Dubv/Documents/chatbot/Capstone-Chatbot/mlchatbot/model.tflearn")) 
+    except: """
+    
+       
+    model.fit(training, output, n_epoch=1250, batch_size=32, show_metric=True)
+    model.save(os.path.abspath("C:/Users/Johnnie5Dubv/Documents/chatbot/Capstone-Chatbot/mlchatbot/model.tflearn"))
 
 
 def bag_of_words(s, words):
     bag = [0 for _ in range(len(words))]
 
     s_words = nltk.word_tokenize(s)
-    s_words = [stemmer.stem(word.lower()) for word in s_words]
+    s_words = [stemmer.stem(word.lower()) for word in s_words if word not in set(stopwords.words("english")) and word != '?']
 
     for se in s_words:
         print(se)
@@ -173,6 +167,30 @@ def bag_of_words(s, words):
     return numpy.array(bag)
 
 
+chatbot()
+
+window = Tk()
+window.title("Chatbot App")
+window.configure(bg= '#B8BCFF')
+window.geometry('1300x900')
+window.bind('<Return>', hitenter)
+helv36 = tkFont.Font(family='Helvetica', size=20, weight='bold')
+lbl1 = scrolledtext.ScrolledText(window,width=80,height=20,wrap=WORD)
+lbl1["font"] = helv36
+lbl1.grid(column=0,padx=20,pady=10,row=1,columnspan = 3,sticky=NW)
+
+txt1 = Entry(window, width=50)
+txt1["font"] = helv36
+txt1.grid(column=0,padx=20,pady=10, row=2)
+btn = Button(window, text="Chat",padx = 45, pady = 20, command=clicked)
+btn['font'] = helv36
+btn.configure(bg='#D1D4FF')
+btn.grid(column=1, ipadx=0,ipady=0, row=2)
+
+btnexit = Button(window, text="Exit",padx = 45, pady = 20, command=lambda: window.destroy())
+btnexit['font'] = helv36
+btnexit.configure(bg='#D1D4FF')
+btnexit.grid(column=2, row=2)
 lbl1.insert(INSERT,"Start talking with the bot (type quit to stop)!\n")
 txt1.focus()
 window.mainloop()
